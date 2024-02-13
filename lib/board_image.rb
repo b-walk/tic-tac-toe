adef452ae9dac49b180b53edb3c2f6b16bdd920b
Blake <blameswalker@gmail.com>
2024-02-13 02:09:00 -0800

    Implement #row_to_s method; allow formatted printing of board data via #view

--- lib/board_image.rb
+++ lib/board_image.rb
@@ -7,4 +7,16 @@ class BoardImage
   def initialize
     @icons = %w[a b c d e f g h i]
   end
+
+  def view
+    puts row_to_s(icons[0..2])
+    puts "---+---+---"
+    puts row_to_s(icons[3..5])
+    puts "---+---+---"
+    puts row_to_s(icons[6..8])
+  end
+
+  def row_to_s(row)
+    " #{row[0]} | #{row[1]} | #{row[2]} "
+  end
 end
