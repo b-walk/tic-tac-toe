e8f270e5ab8d4bd2a4f223636b722474ec1094df
Blake <blameswalker@gmail.com>
2024-02-15 01:12:17 -0800

    Remove BoardImage class; migrate methods to Board to condense code

--- lib/board.rb
+++ lib/board.rb
@@ -4,10 +4,24 @@ class Board
   attr_reader :squares
 
   def initialize
-    @squares = Array.new(9)
+    @squares = %w[a b c d e f g h i]
   end
 
   def add_mark(index, mark)
     squares[index] = mark
+  end
+
+  def view
+    puts "\n", row_to_s(squares[0..2])
+    puts "---+---+---"
+    puts row_to_s(squares[3..5])
+    puts "---+---+---"
+    puts row_to_s(squares[6..8]), "\n"
+  end
+
+  private
+
+  def row_to_s(row)
+    " #{row[0]} | #{row[1]} | #{row[2]} "
   end
 end
--- main.rb
+++ main.rb
@@ -1,7 +1,11 @@
 #frozen_string_literal: true
 
-require_relative 'lib/board.rb'
+require_relative 'lib/board'
 
 board = Board.new
+
 board.add_mark(0, 'X')
-p board.squares
+board.view
+
+board.add_mark(4, 'O')
+board.view
